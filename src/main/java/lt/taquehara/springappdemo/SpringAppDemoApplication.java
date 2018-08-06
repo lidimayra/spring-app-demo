9ccf120dfc1256d9117bde27d05ccb343b16d4e8
Lidiane Taquehara <lidi.mayra@gmail.com>
2018-08-06 17:22:06 -0300

    Initial commit

---
+++ src/main/java/lt/taquehara/springappdemo/SpringAppDemoApplication.java
@@ -0,0 +1,12 @@
+package lt.taquehara.springappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppDemoApplication.class, args);
+    }
+}
---
+++ src/main/java/lt/taquehara/springappdemo/controller/Dashboard.java
@@ -0,0 +1,19 @@
+package lt.taquehara.springappdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Dashboard {
+
+    @RequestMapping("/generic")
+    public String genericDashboard() {
+       return "Hello!";
+    }
+
+    @RequestMapping("/custom")
+    public String customDashboard(@RequestParam(value ="name") String name) {
+        return "Welcome " + name + "!";
+    }
+}
